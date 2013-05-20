3bdd3bfafdb8fd8f2c25c61acf00d55dd8a5bb46
Jardel <jardelrodrigues93@gmail.com>
2013-05-20 16:33:00 -0300

    commitando

--- src/main/java/org/cejug/cc_jsf/bean/ProdutoBean.java
+++ src/main/java/org/cejug/cc_jsf/bean/ProdutoBean.java
@@ -1,5 +1,11 @@
 package org.cejug.cc_jsf.bean;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+
+@ManagedBean(name="produtoBean")
+@RequestScoped
 public class ProdutoBean {
 
 }
