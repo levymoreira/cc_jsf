98a58b496d9f42cde35389d0a110b2dc4c93aaab
Levy Moreira <levymoreira.ce@gmail.com>
2013-06-10 08:44:26 -0300

    Adicionando mais comentários.

--- src/main/java/org/cejug/cc_jsf/dao/EntityManagerUtil.java
+++ src/main/java/org/cejug/cc_jsf/dao/EntityManagerUtil.java
@@ -3,13 +3,28 @@ package org.cejug.cc_jsf.dao;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+/**
+ * Esta classe gerência o objeto EntityManagerFactory, é usado o padrão Singleton garantindo uma
+ * única instância do objeto em toda a aplicação.
+ *
+ */
 public class EntityManagerUtil {
 	
+	/**
+	 * Classe responsável por criar (fabricar) EntityManagers
+	 */
 	private static EntityManagerFactory entityManagerFactory = null;
 	
 	//Construtor Padrão sem argumentos
 	public EntityManagerUtil(){}
 	
+	/**
+	 * Para criar a instância de EntityManagerFactory é usado o arquivo persistence.xml que contém as configurações do 
+	 * banco de dados a ser utilizado, dentro deste arquivo de configuração é definido um nome para a propriedade
+	 * persistence unit, este nome é passado na criacão do EntityManagerFactory.
+	 * 
+	 * @return
+	 */
 	public static EntityManagerFactory getEntityManagerFacotory(){
 		if(entityManagerFactory == null){
 			entityManagerFactory = Persistence.createEntityManagerFactory("cc_jsf_persistence_unit");
--- src/main/java/org/cejug/cc_jsf/dao/ProdutoDAO.java
+++ src/main/java/org/cejug/cc_jsf/dao/ProdutoDAO.java
@@ -8,17 +8,37 @@ import javax.persistence.Query;
 
 import org.cejug.cc_jsf.pojo.Produto;
 
+/**
+ * Classe responsável por fazer as operações básicas de CRUD com a classe produto
+ *
+ */
 public class ProdutoDAO {
 	
+	/**
+	 * O EntityManager é uma API que nos da a possibilidade de realizar as operações de salvar, deletar, e prover
+	 * meios de consulta para gerenciar uma entidade (ex: neste caso a entidade é o produto).
+	 * 
+	 */
 	private EntityManager entityManager = null;
 	
 	//Construtor
 	
+	/**
+	 * Ao instanciar a classe ProdutoDAO o EntityManager é instanciado.
+	 * A classe EntityManagerUtil retorna uma fábrica de entity manager que é o responsável por criar 
+	 * instancias de EntityManagar, veja a classe EntityManagerUtil.
+	 * 
+	 */
 	public ProdutoDAO() { 
 		entityManager = EntityManagerUtil.getEntityManagerFacotory().createEntityManager(); 
 	}
 	
-	public void salvarOuAlterar(Produto produto) { //metodo salvar/alterar produto
+	/**
+	 * Salvar ou alterar um produto
+	 * 
+	 * @param produto
+	 */
+	public void salvarOuAlterar(Produto produto) { 
 		entityManager.getTransaction().begin(); //inicia uma transação com o banco
 		if (produto.getDataDeCadastro() == null) { //caso o getDataDeCadastro seja null, retorna a data atual
 			produto.setDataDeCadastro(new Date());
@@ -29,14 +49,24 @@ public class ProdutoDAO {
 		entityManager.getTransaction().commit(); //retorna a transação e faz o commit para o banco
 	}
 
-	public void deletar(Produto produto) { //metodo deleta produto
+	/**
+	 * Deletar um produto
+	 * 
+	 * @param produto
+	 */
+	public void deletar(Produto produto) { 
 		entityManager.getTransaction().begin(); //inicia uma transação com o banco
 		entityManager.remove(produto); //remove o produto
 		entityManager.getTransaction().commit(); //retorna a transação e faz o commit para o banco
 	}
 
+	/**
+	 * Retorna uma lista com todos produtos cadastrados
+	 * 
+	 * @return
+	 */
 	@SuppressWarnings("unchecked")
-	public List<Produto> getTodos() { //metodo lista dos produtos
+	public List<Produto> getTodos() { 
 		Query query = entityManager.createQuery("SELECT p FROM Produto p"); //retorna todos os produtos do banco utilizando a lingaugem JPQL de consulta do JPA
 		return query.getResultList(); //mostra o resultado da lista de produtos cadastro no banco
 	} 
